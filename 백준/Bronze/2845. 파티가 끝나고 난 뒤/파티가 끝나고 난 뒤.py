aef164ba2ca88cf9d95a5b9f8726a0d2c6713511
Kyuwon Choi <132901684+Kyuwony@users.noreply.github.com>
2025-02-02 15:39:05 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.py
@@ -0,0 +1,4 @@
+people, area = map(int, input().split())
+n1, n2, n3, n4, n5 = map(int, input().split())
+
+print(f"{n1 - people*area} {n2 - people*area} {n3 - people*area} {n4 - people*area} {n5 - people*area}")
